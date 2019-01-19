86611481f30350a8e48e81bafacca4bd0e660c25
Kevin Travers <kevtraver1@gmail.com>
2019-01-19 14:21:06 -0500

    Backend lamdba functions set up

--- whiz-app-backend/app.py
+++ whiz-app-backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Welcome to Whiz"
+	return "Welcome to My app"
 
 if __name__=="__main__":
 	app.run()
--- whiz-app-frontend/src/App.js
+++ whiz-app-frontend/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Pipeline is Working for frontend
+            Welcome to Whiz
           </p>
         </header>
       </div>
