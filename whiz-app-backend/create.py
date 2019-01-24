c0c70d807755d02c526e16763a729f5af340c508
Kevin Travers <kevtraver1@gmail.com>
2019-01-23 23:07:09 -0500

    Using Geohash to store,search location of bathrooms. Starting to work on create lambda to generate geohash from longitude and latitude

--- whiz-app-backend/create.py
+++ whiz-app-backend/create.py
@@ -1,10 +1,27 @@
 from flask import Flask,jsonify,request
 
 app = Flask(__name__)
-
-@app.route("/")
+@app.route("/create_bathroom", methods=['GET'])
 def create_bathroom():
-	return "Bathroom created"
+	longitude 	= request.args.get('longitude')
+	latitude 	= request.args.get('latitude')
+	precision 	= 12
+	geo_hash 	= calculate_geo_hash(longitude,latitude,precision)
+
+	return geo_hash
+
+def calculate_geo_hash(longitude,latitude,precision):
+	#validate inputs
+	#initate variables
+	long_max	= 90
+	long_min	= -90
+	lat_max		= 180
+	lat_min		= -180
+	result 		= ""
+	#while len(result) < precision:
+	#
+	return "Geo Hash calculating"
+
 
 if __name__=="__main__":
 	app.run()
