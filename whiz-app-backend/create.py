18f7d7db79f2b77eac2e6854fbbef85234a9bb48
Kevin Travers <kevtraver1@gmail.com>
2019-01-25 21:21:07 -0500

    Added code to create geohash

--- whiz-app-backend/create.py
+++ whiz-app-backend/create.py
@@ -10,17 +10,57 @@ def create_bathroom():
 
 	return geo_hash
 
-def calculate_geo_hash(longitude,latitude,precision):
-	#validate inputs
-	#initate variables
-	long_max	= 90
-	long_min	= -90
-	lat_max		= 180
-	lat_min		= -180
-	result 		= ""
-	#while len(result) < precision:
-	#
-	return "Geo Hash calculating"
+def geo_hash(latidue,longitide):
+    #validate input
+    precession = 12
+    geohash = ""
+    max_lat = 90.0
+    min_lat = -90.0
+    max_long = 180.0
+    min_long = -180.0
+    base32 = '0123456789bcdefghjkmnpqrstuvwxyz'
+    evenBit = True
+    bits = 0
+    index = 0
+    result = ""
+    while len(geohash)<precession:
+        if evenBit:
+            #bisect E-W longitude
+            mid_long = (min_long+max_long)/2
+            if longitide >= mid_long:
+                index = index*2+1
+                min_long = mid_long
+            else:
+                index = index*2
+                max_long = mid_long
+        else:
+            #bisect N-S latidue
+            mid_lat = (min_lat+max_lat)/2
+            if latidue >= mid_lat:
+                index = index*2+1
+                min_lat = mid_lat
+            else:
+                index = index*2
+                max_lat = mid_lat
+        evenBit = not evenBit
+        
+        
+        bits += 1
+        if bits == 5:
+            result += hash_to_binary(index)
+            geohash += base32[index]
+            bits = 0
+            index = 0
+        
+    return result
+def hash_to_binary(value):
+    bit = ""
+    bits = 4
+    for exp in range(bits,-1,-1):
+        bit += str(value//(2**exp))
+        if value//(2**exp):
+            value -= 2**exp
+    return bit
 
 
 if __name__=="__main__":
